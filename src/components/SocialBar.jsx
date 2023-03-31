02cdf7c2f3db05bdc71450cc5f1813c6287c0010
Alex <alex.salerno@me.com>
2023-03-31 16:43:49 -0700

    add github social

--- src/components/SocialBar.jsx
+++ src/components/SocialBar.jsx
@@ -6,6 +6,7 @@ export default class SocialBar extends React.Component {
     render() {
         return (
             <div style={{...this.props.style}}>
+                <Button style={buttonStyle} onClick={() => {window.open('https://github.com/8bitAlex','_newtab');}}><img src="../img/github_64.png" alt="Github icon"/></Button>
                 <Button style={buttonStyle} onClick={() => {window.open('https://www.linkedin.com/in/8bitalex/','_newtab');}}><img src="../img/linkedin_64.png" alt="LinkedIn icon"/></Button>
             </div>
         );
