b4d2fbc7f99d1c4427fa43864a1c321603391ab6
Alex <alex.salerno@me.com>
2023-07-31 19:51:13 -0700

    add google analytics with pageviews

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,30 @@
 import '@/style/globals.scss'
 import type { Metadata } from 'next'
+import { useEffect } from 'react'
+import Analytics from 'react-ga'
 
 export const metadata: Metadata = {
   title: "Alex Salerno's Site",
   description: "",
 }
 
+const GTAG = "G-LH0F01NP3E"
+Analytics.initialize(GTAG)
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  // pageview event on component mount
+  useEffect(() => {
+    Analytics.pageview(window.location.pathname)
+  })
+
   return (
     <html lang="en">
+      <head></head>
       <body>{children}</body>
     </html>
   )
