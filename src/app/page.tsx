2808ad592fd4c95bd82949486602486a7d02fe40
Alex <alex.salerno@me.com>
2023-07-31 19:56:54 -0700

    move to client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,20 @@
 import Button from "@/components/Button";
 import { zoom } from "@/lib/utils";
 import { useRouter } from "next/navigation";
-import { CSSProperties } from "react";
+import { CSSProperties, useEffect } from "react";
+import Analytics from 'react-ga'
+
+const GTAG = "G-LH0F01NP3E"
+Analytics.initialize(GTAG)
 
 export default function Page() {
   const router = useRouter()
-  
+
+  // pageview event on component mount
+  useEffect(() => {
+    Analytics.pageview(window.location.pathname)
+  })
+
   return (
     <Button>
       <p style={style} onClick={() => zoom('/home', router)}>Press Start</p>
